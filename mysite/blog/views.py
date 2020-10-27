18f8dc1a1cf2b2eaf1e071cca837b6d6bf812a6f
khaledhamada <khaledosman737@gmail.com>
2020-10-27 19:24:28 +0200

    createCommentView_2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -99,6 +99,7 @@ def add_comment_to_post(request , pk):
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk)
     comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
 
 
 
